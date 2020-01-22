ca52db87025f7a81c0a1ee599bd43b8f1b434526
Zachary Seguin <zachary.seguin@canada.ca>
2020-01-22 14:37:29 -0500

    feat(elastic): Upgrade operator to 1.0.0

--- main.tf
+++ main.tf
@@ -18,6 +18,9 @@ resource "null_resource" "dependency_getter" {
 }
 
 resource "null_resource" "eck_init" {
+  triggers = {
+    hash = filesha256("${path.module}/config/elasticsearch-operator.yml")
+  }
 
   provisioner "local-exec" {
     command = "kubectl -n ${var.kubectl_namespace} apply -f ${"${path.module}/config/elasticsearch-operator.yml"}"
